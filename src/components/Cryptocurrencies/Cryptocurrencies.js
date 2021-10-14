cf378ef1ec89ceae8aa027b3ad392d84a200eb73
simonchen92 <simon.k.chen92@gmail.com>
2021-10-14 14:21:56 -0400

    feat(crypto-page): created crypto component & styling

--- src/components/Cryptocurrencies/Cryptocurrencies.js
+++ src/components/Cryptocurrencies/Cryptocurrencies.js
@@ -1,11 +1,68 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import millify from "millify";
+import { Link } from "react-router-dom";
+import { Card, Row, Col, Input } from "antd";
 
-const Cryptocurrencies = () => {
-    return (
-        <div>
-            Cryptocurrencies
+import Loader from "../Loader/Loader";
+import { useGetCryptosQuery } from "../../services/cryptoApi";
+import "./Cryptocurrencies.css";
+
+const Cryptocurrencies = ({ simplified }) => {
+  const count = simplified ? 10 : 100;
+  const { data: cryptosList, isFetching } = useGetCryptosQuery(count);
+  const [cryptos, setCryptos] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    const filteredData = cryptosList?.data?.coins.filter((coin) =>
+      coin.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
+    setCryptos(filteredData);
+  }, [cryptosList, searchTerm]);
+
+  if (isFetching) return <Loader />;
+
+  return (
+    <>
+      {!simplified && (
+        <div className="search-crypto">
+          <Input
+            placeholder="Search Cryptocurrency"
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
         </div>
-    )
-}
+      )}
+      <Row gutter={[32, 32]} className="crypto-card-container">
+        {cryptos?.map((coin) => (
+          <Col xs={24} sm={12} lg={6} className="crypto-card" key={coin.id}>
+            <Link to={`/crypto/${coin.id}`}>
+              <Card
+                title={`${coin.rank}. ${coin.name}`}
+                extra={
+                  <img
+                    className="crypto-image"
+                    src={coin.iconUrl}
+                    alt={`${coin.name}`}
+                  />
+                }
+                hoverable
+              >
+                <p>Price: {millify(coin.price)}</p>
+                <p>Market Cap: {millify(coin.marketCap)}</p>
+                <p>
+                  Daily Change:{"  "}
+                  <span style={{ color: coin.change > 0 ? "green" : "red" }}>
+                    {millify(coin.change)}%
+                  </span>
+                </p>
+              </Card>
+            </Link>
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
+};
 
-export default Cryptocurrencies
+export default Cryptocurrencies;
